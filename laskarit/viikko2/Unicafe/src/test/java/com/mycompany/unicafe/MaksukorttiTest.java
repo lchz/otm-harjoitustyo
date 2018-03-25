4fb5d7d18ae73d838fecef9a6c9369225ccb0f4c
lchz <chenhui_zhao@outlook.com>
2018-03-25 15:08:43 +0300

    Testit lisätty Unicafeen

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,38 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoOikeinAlussa() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanSummaOikeinLataamisenJalkeen() {
+        kortti.lataaRahaa(100);
+        
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenTarpeellisestaSaldosta() {
+        kortti.lataaRahaa(100);
+        boolean riittava = kortti.otaRahaa(100);
+        
+        assertEquals("saldo: 0.10", kortti.toString());
+        assertTrue(riittava == true);
+    }
+    
+    @Test
+    public void rahanOttaminenPuuttuvastaSaldosta() {
+        boolean riittava = kortti.otaRahaa(100);
+        
+        assertEquals("saldo: 0.10", kortti.toString());
+        assertTrue(riittava == false);
+    }
+    
+//    @Test
+//    public void onkoRiittavaRaha() {
+//        kortt
+//    }
 }
