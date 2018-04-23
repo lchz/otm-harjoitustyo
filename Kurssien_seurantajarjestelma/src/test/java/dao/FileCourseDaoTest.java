c672410a396c9bc76523195756f91dc4861413a7
lchz <chenhui_zhao@outlook.com>
2018-04-23 20:50:32 +0300

    created folder test/java/dao/

---
+++ Kurssien_seurantajarjestelma/src/test/java/dao/FileCourseDaoTest.java
@@ -0,0 +1,83 @@
+package dao;
+
+import domain.FakeUserDao;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import otm.kurssien_seurantajarjestelma.dao.CourseDao;
+import otm.kurssien_seurantajarjestelma.dao.FileCourseDao;
+import otm.kurssien_seurantajarjestelma.dao.FileUserDao;
+import otm.kurssien_seurantajarjestelma.dao.UserDao;
+import otm.kurssien_seurantajarjestelma.domain.Course;
+import otm.kurssien_seurantajarjestelma.domain.User;
+
+public class FileCourseDaoTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();  
+    
+    File userFile;  
+    CourseDao dao;  
+    
+    @Before
+    public void setUp() throws Exception {
+        userFile = testFolder.newFile("testfile_users.txt");  
+        UserDao userDao = new FakeUserDao();
+        userDao.create(new User(1, "Nikolas", "Nick", "nick@365.com", "nick123"));
+        
+        try (FileWriter file = new FileWriter(userFile.getAbsolutePath())) {
+            file.write("1; matikka; false; Nick; Nikolas\n");
+        }
+        
+        dao = new FileCourseDao(userFile.getAbsolutePath(), userDao);  
+    }
+    
+    @Test
+    public void coursesAreReadCorrectlyFromFile() {
+        List<Course> courses = dao.getAll();
+        assertEquals(1, courses.size());
+        
+        Course course = courses.get(0);
+        assertEquals("matikka", course.getContent());
+        assertEquals(1, course.getId());
+        assertEquals("Nick", course.getUser().getUsername());
+        assertEquals("Nikolas", course.getUser().getName());
+    }
+    
+    @Test
+    public void courseCanBeSetFinished() throws Exception {
+        dao.setFinished(1);
+        
+        Course course = dao.getAll().get(0);
+        assertTrue(course.isFinished());
+//        assertEquals(1, course.getId());
+    }
+    
+    @Test
+    public void createCoursesAreListed() throws Exception {
+        dao.create(new Course(2, "english", false, new User(2, "Susanna", "susu", "susu@126.com", "susu123")));
+        
+        List<Course> courses = dao.getAll();
+        assertEquals(2, courses.size());
+        
+        Course course = courses.get(1);
+        assertEquals("english", course.getContent());
+        assertFalse(course.isFinished());
+        assertEquals(2, course.getId());
+        assertEquals("Susanna", course.getUser().getName());
+        assertEquals("susu", course.getUser().getUsername());
+    }
+    
+    @After
+    public void tearDown() {
+        userFile.delete();
+    }
+}
---
+++ Kurssien_seurantajarjestelma/src/test/java/dao/FileUserDaoTest.java
@@ -0,0 +1,102 @@
+package dao;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import otm.kurssien_seurantajarjestelma.dao.FileUserDao;
+import otm.kurssien_seurantajarjestelma.dao.UserDao;
+import otm.kurssien_seurantajarjestelma.domain.User;
+
+public class FileUserDaoTest {
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+    
+    File fakeUserFile;
+    UserDao dao;
+    
+    @Before
+    public void setUp() throws IOException, Exception {
+        fakeUserFile = testFolder.newFile("testfile_users.txt");
+        
+        try (FileWriter file = new FileWriter(fakeUserFile.getAbsolutePath())) {
+            file.write("1; Nikolas; Nick; nick@365.com; nick123\n");
+        } catch (Exception e) {
+//            FileWriter file = new FileWriter(new File(filee));
+            Throwable cause = e.getCause();
+        }
+        
+        dao = new FileUserDao(fakeUserFile.getAbsolutePath());
+    }
+    
+    @Test
+    public void usersAreReadCorrectlyFromFile() {
+        List<User> users = dao.getAll();
+        assertEquals(1, users.size());
+        
+        User user = users.get(0);
+        assertEquals("Nikolas", user.getName());
+        assertEquals("Nikolas", user.getName());
+        assertEquals("Nick", user.getUsername());
+        assertEquals("nick@365.com", user.getEmail());
+        assertEquals("nick123", user.getPassword());
+    }
+    
+    @Test
+    public void existingUserIsFoundByUsername() {
+        User user = dao.findByUsername("Nick");
+        assertEquals("Nikolas", user.getName());
+        assertEquals("Nick", user.getUsername());
+        assertEquals("nick@365.com", user.getEmail());
+        assertEquals("nick123", user.getPassword());
+    }
+    
+    @Test
+    public void existingUserIsFoundByName() {
+        User user = dao.findByName("Nikolas");
+        assertEquals("Nikolas", user.getName());
+        assertEquals("Nick", user.getUsername());
+        assertEquals("nick@365.com", user.getEmail());
+        assertEquals("nick123", user.getPassword());
+    }
+    
+    @Test
+    public void nonExistingUserIsFound() {
+        User user = dao.findByUsername("hellas");
+        assertEquals(null, user);
+    }
+    
+    @Test
+    public void passwordMatchesUser() {
+        User user = dao.findByUsername("Nick");
+        assertTrue("nick123", dao.passwordMatches(user, user.getPassword()));
+    }
+    
+    @Test
+    public void savedUserIsFound() throws Exception {
+        User newUser = new User(2, "Arto Hellas", "hellas", "hellas@365.com", "hellas123");
+        dao.create(newUser);
+        
+        User user = dao.findByName("Arto Hellas");
+        assertEquals("Arto Hellas", user.getName());
+        assertEquals("hellas", user.getUsername());
+        assertEquals("hellas@365.com", user.getEmail());
+    }
+    
+//    @Test
+//    public void catchedExceptionInConstructor() {
+//        when()
+//    }
+    
+    @After
+    public void tearDown() {
+        fakeUserFile.delete();
+    }
+    
+}
