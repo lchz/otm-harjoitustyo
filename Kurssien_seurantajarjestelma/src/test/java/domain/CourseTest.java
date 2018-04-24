d35d04d383114d97ac9c62c951c19f7899060b75
lchz <chenhui_zhao@outlook.com>
2018-04-24 20:10:31 +0300

    updated tests

--- Kurssien_seurantajarjestelma/src/test/java/domain/CourseTest.java
+++ Kurssien_seurantajarjestelma/src/test/java/domain/CourseTest.java
@@ -1,9 +1,6 @@
 package domain;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import otm.kurssienseurantajarjestelma.domain.Course;
